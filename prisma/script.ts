0a6971e50e4176b922681ab63b1445c61739073e
me <me>
2022-10-22 11:37:29 -0700

    i really didnt add anything huh

---
+++ prisma/script.ts
@@ -0,0 +1,21 @@
+// OOOO I LOVE PASTING WOOOOOO
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+	console.log("hello")
+	const shops = await prisma.coffeeShop.findMany()
+	console.log(shops)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
