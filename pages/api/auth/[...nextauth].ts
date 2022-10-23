91b21d8651afeff5d3b62559eee0954ce4af9c45
NotMyPersonalAccount <kevinzj710@gmail.com>
2022-10-22 18:24:46 -0700

    pls work

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,6 +7,7 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-  ]
+  ],
+  secret: process.env.SECRET as string,
 };
 export default NextAuth(authOptions);
